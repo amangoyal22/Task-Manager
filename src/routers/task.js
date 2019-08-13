26341d80a7ea0ba3890fa6f291834d758ac920c9
amangoyal22 <aman.goyalwd@gmail.com>
2019-08-13 22:52:50 +0530

    pagination

--- src/routers/task.js
+++ src/routers/task.js
@@ -20,13 +20,24 @@ router.post('/tasks',auth,async (req,res)=>{
     }
 })
 
-// specific task
-router.get('/tasks/:id',auth,async (req,res)=>{ 
-    const _id = req.params.id
+// specific task ? completed = true & skip = 1 & limit = 1
+router.get('/tasks',auth,async (req,res)=>{ 
+    //const _id = req.params.id
+    const match = {}
+    if(req.query.completed) {
+        match.completed = req.query.completed === 'true'
+    }
     try {
         //const task = await Task.findById(_id);
         //const task = await Task.findOne({_id,owner: req.user._id})
-        await req.user.populate('tasks').execPopulate()
+        await req.user.populate({
+            path : 'tasks',
+            match,
+            options : {
+                limit : parseInt(req.query.limit),
+                limit : parseInt(req.query.skip)
+            }
+        }).execPopulate()
         if(!task) {
             return res.status(404).send(req.user.tasks)
         }
